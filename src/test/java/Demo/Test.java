3370fce7e37b8a77ef3cc174ed167d7e73ad0a6f
Sahil Sharma <sharmasahil201912@gmail.com>
2026-02-25 19:52:22 +0530

    Added a new test file

--- src/test/java/Demo/Test.java
+++ src/test/java/Demo/Test.java
@@ -5,6 +5,12 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi");
+		
+	}
+	public static void name() {
+		
+		System.out.println("demo");
+		
 	}
 
 }
